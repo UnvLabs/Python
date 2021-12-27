9b03489c8f5ff2b424f50452205018f3c1d926c0
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-27 19:33:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,71 +11,54 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name='unv',  # Required
-
-    version='2.0.0',  # Required
-
-    description='Python implementation of the Unv programiing language',  # Optional
-
+    name="unv",  # Required
+    version="2.0.0",  # Required
+    description="Python implementation of the Unv programiing language",  # Optional
     long_description=long_description,  # Optional
-
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    url='https://github.com/unvlabs/python',  # Optional
-
-    author='Kavindu Santhusa',  # Optional
-
-    author_email='ksengine.github@gmail.com',  # Optional
-
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="https://github.com/unvlabs/python",  # Optional
+    author="Kavindu Santhusa",  # Optional
+    author_email="ksengine.github@gmail.com",  # Optional
     # For a list of valid classifiers, see https://pypi.org/classifiers/
     classifiers=[  # Optional
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: MIT License',
-
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        'Programming Language :: Python :: 3 :: Only',
+        "Programming Language :: Python :: 3 :: Only",
     ],
-
-    keywords='python, unv, programming, language, implementation, compiler',  # Optional
-
-    package_dir={'': 'src'},  # Optional
-
-    packages=find_packages(where='src'),  # Required
-
-    python_requires='>=3.6, <4',
-
+    keywords="python, unv, programming, language, implementation, compiler",  # Optional
+    package_dir={"": "src"},  # Optional
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.6, <4",
     entry_points={  # Optional
-        'console_scripts': [
-            'unv=unv:main',
+        "console_scripts": [
+            "unv=unv:main",
         ],
     },
-
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/unvlabs/python/issues',
+        "Bug Reports": "https://github.com/unvlabs/python/issues",
         #'Funding': 'https://donate.pypi.org',
         #'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/unvlabs/python/',
+        "Source": "https://github.com/unvlabs/python/",
     },
 )
