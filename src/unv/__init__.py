ad0f45e124635af09da2a82d579722af794b97fe
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-27 19:34:29 +0530

    Update __init__.py

--- src/unv/__init__.py
+++ src/unv/__init__.py
@@ -11,7 +11,9 @@ FUNCTION = re.compile(r"^(\s*(?:async\s+)?)function(\*?\s+.+)", re.MULTILINE)
 def compile(input):
     without_comments = re.sub(COMMENTS, "", input)
     strings_converted = re.sub(
-        STRINGS, lambda match: match.group().replace("\n", "\\n"), without_comments
+        STRINGS,
+        lambda match: match.group().replace("\n", "\\n"),
+        without_comments,
     )
     added_colons = re.sub(
         STATEMENT, lambda match: match.group() + ":", strings_converted
