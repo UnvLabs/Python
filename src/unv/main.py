e3e1a12e35ea907c188c6e679e487a8110f30ebd
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 12:05:15 +0530

    Update main.py

--- src/unv/main.py
+++ src/unv/main.py
@@ -1,10 +1,19 @@
 from code import InteractiveConsole
+from argparse import ArgumentParser
+from compile import compile
+
+try:
+    import readline
+except ImportError:
+    pass
 
 INTRO = """
 |     |  
 |     | |/ \ \  /
  \___/  |   | \/   Compiler
 """
+
+
 class UnvConsole(InteractiveConsole):
     def push(self, line):
         self.buffer.append(line)
@@ -14,38 +23,17 @@ class UnvConsole(InteractiveConsole):
             self.resetbuffer()
         return more
 
-def main(count, name):
-    """Unv programming language"""
-    console = UnvConsole({})
-    try:
-        import readline
-    except ImportError:
-        pass
-    console.interact("Interactive Unv Console.")
 
 def main():
-    parser = argparse.ArgumentParser(description=INTRO, formatter_class=RawTextHelpFormatter)
-    parser.add_argument('files', nargs='+', help='One or more source files that you would like converted.')
-    parser.add_argument('-o', '--out-lang', help='Specify the desired output language. Defaults to JavaScript.',
-                        dest='out_lang', type=str, choices=("py", "js"), default="")
-    parser.add_argument('-e', '--out-ext', help='Specify the desired output files extension (such as py or js). '
-                        'Default is baesd on lang', dest='out_ext', type=str,)
-    parser.add_argument('-i' '--in-ext', help='Specify the extension of the files to parse. Defualt is .jiphy',
-                        default="jiphy", dest="in_ext")
-    parser.add_argument('-rc', '--recursive', dest='recursive', action='store_true',
-                        help='Recursively look for files to convert')
-    parser.add_argument('-od', '--out-dir', dest='out_dir', default="",
-                        help="Specify in which directory files should be outputed")
-    parser.add_argument('-d', '--diff', dest='diff', default=False, action='store_true',
-                        help="Produce a diff that would result in running jiphy, "
-                             "without actually performing any changes")
-    parser.add_argument('-c', '--conform', dest='conform', default=False, action='store_true',
-                        help="Conform all code within passed in files to the format implied by its extension")
-    parser.add_argument('-v', '--version', dest='version', action='version',
-                        version="Jiphy v.{0}".format(__version__))
-    
-    arguments = dict((key, value) for (key, value) in itemsview(vars(parser.parse_args())))
+    parser = ArgumentParser(description=INTRO)
+    parser.add_argument("file", help="An Unv source file to run")
+    args = parser.parse_args()
 
+    if args.file:
+        exec(open(args.file, "r").read(), {})
+    else:
+        console = UnvConsole({"true": True, "false": False})
+        console.interact("Interactive Unv Console.")
 
 
 if __name__ == "__main__":
