6090193ff784ff8cc16c67f0c80132860cd3a735
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 14:42:09 +0530

    Create setup.py

---
+++ build/setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["encodings"], "excludes": []}
+
+if sys.platform == "win32":
+    build_exe_options["excludes"] = ["readline"]
+
+setup(
+    name = "unv",
+    version = "0.0.1",
+    description = "Unv comman line tool",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("unv.py", base=None)]
+)
