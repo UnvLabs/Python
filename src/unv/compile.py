ae845f4b432be80e2c33da6e05e80cafee36ee51
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-06 18:36:52 +0530

    Update compile.py

--- src/unv/compile.py
+++ src/unv/compile.py
@@ -4,10 +4,7 @@ from io import BytesIO
 
 COMMENTS = r"###[\s\S]*?###|#.*"
 STRINGS = r""""(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*'"""
-STATEMENT = re.compile(
-    r"^\s*()\s+.+", re.MULTILINE
-)
-FUNCTION = re.compile(r"^(\s*(?:async\s+)?)function(\*?\s+.+)", re.MULTILINE)
+
 
 def compile(input):
     without_comments = re.sub(COMMENTS, "", input)
@@ -16,30 +13,46 @@ def compile(input):
         lambda match: match.group().replace("\n", "\\n"),
         without_comments,
     )
-    tokens = tokenize(BytesIO(strings_converted.encode('utf-8')).readline)
+    tokens = tokenize(BytesIO(strings_converted.encode("utf-8")).readline)
 
     code = ""
     sqb = 0
     parens = 0
     braces = 0
-    add_colon = false
+    add_colon = False
     for token in tokens:
         if token.start != token.end:
             if token.type == 1 and token.string == "function":
                 code += "def"
-                add_colon = true
+                add_colon = True
             else:
-                if token.type == 1 and token.string in "if|else|switch|try|catch|class|do|while|for".split("|"):
-                    add_colon = true
+                if (
+                    token.type == 1
+                    and token.string
+                    in "if|else|switch|try|catch|class|do|while|for".split("|")
+                ):
+                    add_colon = True
                 if token.type == 54:
-                    if token.string == "(": parens +=1
-                    else if token.string == ")": parens -= 1
-                    else if token.string == "[": sqb += 1
-                    else if token.string == "]": sqb -= 1
-                    else if token.string == "{": braces += 1
-                    else if token.string == "}": braces -= 1
-                if add_colon and token.type == NEWLINE and not sqb and not parens and not braces:
+                    if token.string == "(":
+                        parens += 1
+                    elif token.string == ")":
+                        parens -= 1
+                    elif token.string == "[":
+                        sqb += 1
+                    elif token.string == "]":
+                        sqb -= 1
+                    elif token.string == "{":
+                        braces += 1
+                    elif token.string == "}":
+                        braces -= 1
+                if (
+                    add_colon
+                    and token.type == NEWLINE
+                    and not sqb
+                    and not parens
+                    and not braces
+                ):
                     code += ":"
-                    add_colon = false
-                        
+                    add_colon = False
                 code += token.string
+    return code
