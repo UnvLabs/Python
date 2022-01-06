483f96347b4498f274640ee84da11640c245d38b
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-06 19:23:26 +0530

    Update compile.py

--- src/unv/compile.py
+++ src/unv/compile.py
@@ -1,9 +1,92 @@
 import re
-from tokenize import tokenize, NEWLINE
+from tokenize import (
+    tokenize,
+    ENCODING,
+    ENDMARKER,
+    INDENT,
+    DEDENT,
+    NEWLINE,
+    NL,
+    NAME,
+    OP,
+    Untokenizer,
+)
 from io import BytesIO
 
 COMMENTS = r"###[\s\S]*?###|#.*"
 STRINGS = r""""(?:\\["\\]|[^"\\])*"|'(?:\\['\\]|[^'\\])*'"""
+
+
+class UnvUntokenizer(Untokenizer):
+    def untokenize(self, iterable):
+        it = iter(iterable)
+        indents = []
+        startline = False
+        sqb = 0
+        parens = 0
+        braces = 0
+        add_colon = False
+        for t in it:
+            if len(t) == 2:
+                self.compat(t, it)
+                break
+            tok_type, token, start, end, line = t
+            if tok_type == NAME:
+                if token == "function":
+                    token = "def"
+                    add_colon = True
+                if token in "if|else|switch|try|catch|class|do|while|for".split("|"):
+                    add_colon = True
+            elif tok_type == OP:
+                if token == "(":
+                    parens += 1
+                elif token == ")":
+                    parens -= 1
+                elif token == "[":
+                    sqb += 1
+                elif token == "]":
+                    sqb -= 1
+                elif token == "{":
+                    braces += 1
+                elif token == "}":
+                    braces -= 1
+            elif (
+                add_colon
+                and tok_type == NEWLINE
+                and not sqb
+                and not parens
+                and not braces
+            ):
+                token += ":"
+                add_colon = False
+
+            if tok_type == ENCODING:
+                self.encoding = token
+                continue
+            if tok_type == ENDMARKER:
+                break
+            if tok_type == INDENT:
+                indents.append(token)
+                continue
+            elif tok_type == DEDENT:
+                indents.pop()
+                self.prev_row, self.prev_col = end
+                continue
+            elif tok_type in (NEWLINE, NL):
+                startline = True
+            elif startline and indents:
+                indent = indents[-1]
+                if start[1] >= len(indent):
+                    self.tokens.append(indent)
+                    self.prev_col = len(indent)
+                startline = False
+            self.add_whitespace(start)
+            self.tokens.append(token)
+            self.prev_row, self.prev_col = end
+            if tok_type in (NEWLINE, NL):
+                self.prev_row += 1
+                self.prev_col = 0
+        return "".join(self.tokens)
 
 
 def compile(input):
@@ -15,44 +98,8 @@ def compile(input):
     )
     tokens = tokenize(BytesIO(strings_converted.encode("utf-8")).readline)
 
-    code = ""
-    sqb = 0
-    parens = 0
-    braces = 0
-    add_colon = False
-    for token in tokens:
-        if token.start != token.end:
-            if token.type == 1 and token.string == "function":
-                code += "def"
-                add_colon = True
-            else:
-                if (
-                    token.type == 1
-                    and token.string
-                    in "if|else|switch|try|catch|class|do|while|for".split("|")
-                ):
-                    add_colon = True
-                if token.type == 54:
-                    if token.string == "(":
-                        parens += 1
-                    elif token.string == ")":
-                        parens -= 1
-                    elif token.string == "[":
-                        sqb += 1
-                    elif token.string == "]":
-                        sqb -= 1
-                    elif token.string == "{":
-                        braces += 1
-                    elif token.string == "}":
-                        braces -= 1
-                if (
-                    add_colon
-                    and token.type == NEWLINE
-                    and not sqb
-                    and not parens
-                    and not braces
-                ):
-                    code += ":"
-                    add_colon = False
-                code += token.string
-    return code
+    ut = UnvUntokenizer()
+    out = ut.untokenize(list(tokens))
+    if ut.encoding is not None:
+        out = out.encode(ut.encoding)
+    return out
